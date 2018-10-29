b0dde533d8565a1ff46bea6ef6c6dbd143e10795
hbb0b0@163.com <hbb0b0@163.com>
2018-10-29 17:56:03 +0800

    Fody App Sync

---
+++ FodyApp/MyClass.cs
@@ -0,0 +1,25 @@
+﻿using MethodTimer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FodyApp
+{
+   
+    public class MyClass:IRun
+    {
+        public void Excute()
+        {
+            this.MyMethod();
+        }
+
+        [Time]
+        public void MyMethod()
+        {
+            //Some code u are curious how long it takes
+            Console.WriteLine("Hello");
+        }
+
+       
+    }
+}
---
+++ FodyApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FodyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRun run = new MyClass();
+            run.Excute();
+
+            Console.ReadLine();
+        }
+    }
+}
