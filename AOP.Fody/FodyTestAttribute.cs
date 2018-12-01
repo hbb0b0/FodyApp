b054c55494e0d528c14a2fb0dad6e2ec32091463
hbb0b0@163.com <hbb0b0@163.com>
2018-12-01 15:06:22 +0800

    注释

--- AOP.Fody/FodyTestAttribute.cs
+++ AOP.Fody/FodyTestAttribute.cs
@@ -10,14 +10,19 @@ namespace AOP.Fody
 
     
     /// <summary>
-    /// 方法拦截
+    /// AOP 方法拦截
     /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Assembly | AttributeTargets.Module)]
     public class MethodInterceptorAttributeAttribute : Attribute, IMethodDecorator
     {
 
-        // instance, method and args can be captured here and stored in attribute instance fields
-        // for future usage in OnEntry/OnExit/OnException
+       
+        /// <summary>
+        /// 方法初始化
+        /// </summary>
+        /// <param name="instance">实例</param>
+        /// <param name="method">方法名称</param>
+        /// <param name="args">调用参数</param>
         public void Init(object instance, MethodBase method, object[] args)
         {
             //TestMessages.Record(string.Format("Init: {0} [{1}]", method.DeclaringType.FullName + "." + method.Name, args.Length));
@@ -29,18 +34,28 @@ namespace AOP.Fody
             Console.WriteLine($"Init methodName:{method.Name} args:{strArgs} ");
         }
 
+        /// <summary>
+        /// 方法进入
+        /// </summary>
         public void OnEntry()
         {
             //TestMessages.Record("OnEntry");
             Console.WriteLine("OnEntry");
         }
 
+        /// <summary>
+        /// 方法退出
+        /// </summary>
         public void OnExit()
         {
             //TestMessages.Record("OnExit");
             Console.WriteLine("OnExit");
         }
 
+        /// <summary>
+        /// 方法异常
+        /// </summary>
+        /// <param name="exception"></param>
         public void OnException(Exception exception)
         {
             //TestMessages.Record(string.Format("OnException: {0}: {1}", exception.GetType(), exception.Message));
